fde7c8ecbeb0fcb43d699bc00606d9c704aed538
GaziAshiq <ashiq121914@gmail.com>
2022-10-12 16:34:33 +0600

    blog added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from "react";
 const Blog = () => {
   return (
     <div className="container mt-5">
-      <div>
+      <div className="mb-4">
         <h2>What is the purpose of react router?</h2>
         <p>
           = <br />
@@ -12,6 +12,26 @@ const Blog = () => {
           application. When a user types a specific URL into the browser, and if
           this URL path matches any 'route' inside the router file, the user
           will be redirected to that particular route.
+        </p>
+      </div>
+      <div className="mb-4">
+        <h2>How does context api works?</h2>
+        <p>
+          = <br />
+          The React Context API is a way for a React app to effectively produce
+          global variables that can be passed around. This is the alternative to
+          "prop drilling" or moving props from grandparent to child to parent,
+          and so on. Context is also touted as an easier, lighter approach to
+          state management using Redux.
+        </p>
+      </div>
+      <div className="mb-4">
+        <h2>what is the use of useRef in react?</h2>
+        <p>
+          = <br />
+          The useRef Hook allows you to persist values between renders. It can
+          be used to store a mutable value that does not cause a re-render when
+          updated. It can be used to access a DOM element directly.
         </p>
       </div>
     </div>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
+  const data = useLoaderData();
+  data.data.map((total) => {
+    console.log(total.total);
+  });
+  const total = data.data.total;
+  console.log(total);
+
   return (
-    <div>
+    <div className="">
       <h2>this is statistics</h2>
     </div>
   );
